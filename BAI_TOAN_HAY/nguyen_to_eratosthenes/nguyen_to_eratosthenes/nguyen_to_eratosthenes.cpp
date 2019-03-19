26d8eb6ccaeee233505cb3963d83c9681888bc32
starlord <manhnguyen16112@gmail.com>
2019-03-19 17:54:08 +0700

    add new file

---
+++ BAI_TOAN_HAY/nguyen_to_eratosthenes/nguyen_to_eratosthenes/nguyen_to_eratosthenes.cpp
@@ -0,0 +1,39 @@
+﻿//sàng số nguyên tố
+#include"pch.h"
+#include<iostream>
+#include<cmath>
+#include<string.h>
+using namespace std;
+void eratos(int n)
+{
+	bool a[1000];
+	for (int i = 0; i <n; i++)
+	{
+		a[i] = 1;
+	}
+	for (int i = 2; i < sqrt(n); i++)
+	{
+		if (a[i] == 1)
+		{
+			for (int j = 2 * i; j < n; j += i)
+			{
+				a[j] = 0;
+			}
+		}
+	}
+	for (int i = 2; i < n; i++)
+	{
+		if (a[i] == 1)
+		{
+			cout << i << " ";
+		}
+	}
+}
+int main()
+{   
+	int n;
+	cout << "nhap n:";
+	cin >> n;
+	eratos(n);
+	return 0;
+}
