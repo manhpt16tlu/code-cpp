a3be8269a13b875f738815eb4a4425f872700987
starlord16112 <manhnguyen16112@gmail.com>
2019-12-16 10:15:20 +0700

    add new file

---
+++ bt5_class/danh_sach_lien_ket/main.cpp
@@ -0,0 +1,81 @@
+﻿//nhập danh sách liên kết đơn 
+#include <iostream>
+#include<string>
+using namespace std;
+//cấu trúc 1 node
+struct node
+{
+	int x;
+	node *lk; // con trỏ để liên kết với nhau
+};
+struct list
+{
+	node *dau;
+	node *cuoi;
+};
+void khoitao(list &l)
+{
+	l.cuoi = NULL;
+	l.dau = NULL;
+}
+node *khoitao(int a)// khởi tạo 1 node để thêm vào danh sách
+{
+	node *p = new node;
+	p->x = a;
+	p->lk = NULL;
+	return p;
+} 
+void themdau(list &l, node *p)
+{
+	if (l.dau == NULL)
+	{
+		l.dau = l.cuoi = p;
+	}
+	else
+	{
+		p->lk = l.dau;//cho con trỏ của node cần thêm trỏ tới nốt đầu
+		l.dau = p;//cập nhật node đầu chính là nốt p;
+	}
+
+}
+void themcuoi(list& l, node *p)
+{
+	if (l.dau == NULL)
+	{
+		l.dau = l.cuoi = p;
+	}
+	else
+	{
+		l.cuoi->lk = p;//cho con trỏ của node cuốitrỏ tới nốt cần thêm
+		l.cuoi = p;//cập nhật p là node cuối
+	}
+
+}
+void hamxuatds(list l)
+{
+	for (node* i = l.dau; i != NULL; i = i->lk)
+	{
+		cout << i->x << endl;
+	}
+}
+int main()
+{
+	list l;
+	khoitao(l);
+	int n;
+	cout << "nhap so node cần thêm: ";
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		node *p;
+		p = khoitao(5);
+		themdau(l, p);
+	}
+	hamxuatds(l);
+	
+
+
+
+
+	return 0;
+}
---
+++ bt5_class/lien_ket_don_SV/main.cpp
@@ -0,0 +1,84 @@
+﻿//tạo danh sách liên kết đơn sinh viên
+#include<iostream>
+#include<string>
+using namespace std;
+//khởi tạo class sinh viên 
+class SV
+{
+private:
+	string ma,name;
+public:
+	SV *lk;
+	string getma()
+	{
+		return ma;
+	}
+	string getname()
+	{
+		return name;
+	}
+	void nhaptt()
+	{
+		cout << "nhap ma sv: ";
+		getline(cin, ma);
+		cout << "nhap ten sinh vien: ";
+		getline(cin, name);
+	}
+
+};
+//khởi tạo danh sách sinh viên
+struct DS
+{
+	SV *dau;
+	SV *cuoi;
+};
+void khoitaods(DS &l)
+{
+	l.dau = NULL;
+	l.cuoi = NULL;
+}
+//khởi tạo node sinh viên
+SV *khoitao()
+{
+	SV *p = new SV;
+	p->nhaptt();
+	return p;
+}
+void themdau(DS &l, SV *p)
+{
+	if (l.dau == NULL)
+	{
+		l.dau = l.cuoi = p;
+	}
+
+	else
+	{
+		p->lk = l.dau;
+		l.dau = p;
+	}
+
+}
+void xuat(DS l)
+{
+	for (SV *i = l.dau; i != NULL; i = i->lk)
+	{
+		cout <<i->getma()<<"-"<< i->getname()<< endl;
+	}
+}
+int main()
+{
+	DS l;
+	khoitaods(l);
+	int n;
+	cout << "nhap so sinh vien can them: ";
+	cin >> n;
+	cin.ignore();
+	for (int i = 0; i < n; i++)
+	{
+		SV *p;
+		p = khoitao();
+		themdau(l, p);
+	}
+	xuat(l);
+	return 0;
+}
